f790955ef6abd92ea6dae7f34f52f0c775b255cd
focusotter <mtliendo@focusotter.com>
2023-08-14 02:51:38 -0500

    copy full json

--- backend/lib/mediaConvert/jobTemplate.ts
+++ backend/lib/mediaConvert/jobTemplate.ts
@@ -16,27 +16,42 @@ export const createVideoDownscaleJob = (
 			OutputGroups: [
 				{
 					Name: 'File Group',
-					OutputGroupSettings: {
-						Type: 'FILE_GROUP_SETTINGS',
-						FileGroupSettings: {
-							Destination: `s3://${props.destinationBucketName}/protected/`,
-						},
-					},
 					Outputs: [
 						{
-							ContainerSettings: { Container: 'MP4' },
+							ContainerSettings: {
+								Container: 'MP4',
+								Mp4Settings: {},
+							},
 							VideoDescription: {
 								CodecSettings: {
 									Codec: 'H_264',
 									H264Settings: {
+										MaxBitrate: 5000000,
 										RateControlMode: 'QVBR',
 										SceneChangeDetect: 'TRANSITION_DETECTION',
 									},
 								},
-								Height: 1080,
 							},
+							AudioDescriptions: [
+								{
+									CodecSettings: {
+										Codec: 'AAC',
+										AacSettings: {
+											Bitrate: 96000,
+											CodingMode: 'CODING_MODE_2_0',
+											SampleRate: 48000,
+										},
+									},
+								},
+							],
 						},
 					],
+					OutputGroupSettings: {
+						Type: 'FILE_GROUP_SETTINGS',
+						FileGroupSettings: {
+							Destination: 's3://cdk-hnb659fds-assets-842537737558-us-east-2/',
+						},
+					},
 				},
 			],
 		},
